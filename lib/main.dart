b22b5a22960373e76b3f5ab1ffc4b99d1c12c1fa
Kathirvel Chandrasekaran <kathirvel.chandrasekaran@cat.com>
2022-03-06 05:43:43 +0530

    feat: supabase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:campus_connect/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(CampusConnect());
+void main() async => {
+      WidgetsFlutterBinding.ensureInitialized(),
+      await dotenv.load(fileName: '.env'),
+      Supabase.initialize(
+        url: dotenv.env["SUPABASE_URL"],
+        anonKey: dotenv.env["SUPABASE_ANON_KEY"],
+        authCallbackUrlHostname: dotenv.env["SUPABASE_CALLBACK_URL"],
+      ),
+      runApp(
+        ProviderScope(
+          child: CampusConnect(),
+        ),
+      )
+    };
